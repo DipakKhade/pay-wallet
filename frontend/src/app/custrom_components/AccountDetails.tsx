279e7c7fb5567cb0af17ba88da5f390de10c3ed1
= <=>
2024-07-31 15:42:32 +0530

    addtowallet done

--- frontend/src/app/custrom_components/AccountDetails.tsx
+++ frontend/src/app/custrom_components/AccountDetails.tsx
@@ -4,6 +4,7 @@ import { BACKEND_URL } from "@/lib/config";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useCookies } from "next-client-cookies";
+import { Card } from "@/components/ui/card";
 
 export default function AccountDetails(){
     const [accountno, SetAccountno] =  useState<string>('')
@@ -13,15 +14,20 @@ export default function AccountDetails(){
     const cookies =useCookies();
     useEffect(()=>{
         (async()=>{
-            const r=await axios.post(`${BACKEND_URL}/api/v1/user/getbalance`,{},{
-                method: "POST",
-                headers: {
-                  authorization: cookies.get('authorization'),
-                }})
-            SetAccountno(r.data.account.id)
-            Setbalance(r.data.account.balance)
-            SetLockedbalance(r.data.account.locked)
-            SetLoading(false)
+            try{
+                const r=await axios.post(`${BACKEND_URL}/api/v1/user/getbalance`,{},{
+                    method: "POST",
+                    headers: {
+                      authorization: cookies.get('authorization'),
+                    }})
+                SetAccountno(r.data.account.id)
+                Setbalance(r.data.account.balance)
+                SetLockedbalance(r.data.account.locked)
+                SetLoading(false)
+            }catch(e){
+
+                SetLoading(false)
+            }
         })();
     },[balance])
     console.log(accountno,balance,lockedbalance)
@@ -32,18 +38,20 @@ export default function AccountDetails(){
         </>
     }
     return <>
-    <main className="border border-green-300">
-        <div>
-            {accountno}
-        </div>
-        <div>
-            {balance}
-        </div>
-        <div>
-            {lockedbalance}
+    <Card className="h-[300px] p-3 mt-4 w-[600px]">
+        <p className="font-semibold text-xl">Wallet details</p>
+        <div className="space-y-4 flex justify-between">
+           <div>Account no :</div> {accountno}
         </div>
 
-    </main>
+        <div className="flex justify-between">
+           <div>Wallet balance :</div> RS {balance}
+        </div>
+        <div className="flex justify-between">
+           <div>locked balance :</div> RS {lockedbalance}
+        </div>
+
+    </Card>
     
     </>
 }
--- frontend/src/app/custrom_components/SideBar.tsx
+++ frontend/src/app/custrom_components/SideBar.tsx
@@ -1,29 +1,46 @@
+'use client';
 import { IoMdHome } from "react-icons/io";
 import { TbTransfer } from "react-icons/tb";
 import { TbTransactionDollar } from "react-icons/tb";
 import { MdAccountBalanceWallet } from "react-icons/md";
 import { HiMiniWallet } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 
 export default function SideBar() {
+  const path=usePathname();
+  const router = useRouter();
+  console.log(path)
   const menu = [
     {
       name: "Home",
+      pathname:'/dashboard',
+      page:'/',
       icon: <IoMdHome />,
     },
     {
       name: "Add to Wallet",
+      pathname:'/dashboard/addtowallet',
+      page:'/dashboard/addtowallet',
       icon: <HiMiniWallet />,
     },
     {
       name: "Transfer",
+      pathname:'/dashboard/transfer',
+      page:'/transfer',
       icon: <TbTransfer />,
     },
     {
       name: "Transactions",
+      pathname:'/dashboard/transactions',
+      page:'/transactions',
       icon: <TbTransactionDollar />,
     },
     {
       name: "Check Balance",
+      pathname:'/dashboard/checkbalance',
+      page:'/checkbalance',
       icon: <MdAccountBalanceWallet />,
     },
   ];
@@ -33,9 +50,11 @@ export default function SideBar() {
         <div>
           <div className="pt-8">
             {menu.map((m, index) => (
-              <div className="flex cursor-pointer p-3 space-x-2" key={index}>
+              <div className={`p-3 space-x-2 ${m.pathname==path ? 'text-green-500' : ''}`} key={index}>
+                <button className="flex cursor-pointer" onClick={()=>router.push(m.page)}>
                 <span className="text-xl">{m.icon}</span>
                 <p key={index}>{m.name}</p>
+                </button>
               </div>
             ))}
           </div>
